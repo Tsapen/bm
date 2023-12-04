48e0236a2d4d1c10cf3b98b6dce599306c307577
FreeTime <freetime@Deniss-MacBook-Pro.local>
2023-12-05 02:15:04 +0400

    remove comments

--- internal/migrator/migrator.go
+++ internal/migrator/migrator.go
@@ -11,20 +11,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// // Executor is an interface that defines the methods required for executing SQL queries.
-// type Executor interface {
-// 	Exec(query string, args ...interface{}) (sql.Result, error)
-// }
-
-// // Migrator represents the migration manager.
-// type Migrator struct {
-// 	// executor       Executor
-// 	m              *migrate.Migrate
-// 	migrationsPath string
-// }
-
 // New creates a new instance of Migrator.
-// func New(migrationsPath string, executor Executor) (*Migrator, error) {
 func ApplyMigrations(migrationsPath string, db *sql.DB) error {
 	driver, err := postgres.WithInstance(db, &postgres.Config{
 		SchemaName: "public",
@@ -44,29 +31,3 @@ func ApplyMigrations(migrationsPath string, db *sql.DB) error {
 
 	return nil
 }
-
-// // Apply reads and executes SQL migrations from the specified file path.
-// func (m *Migrator) ApplyDeprecated() error {
-// 	// Read migration content from the specified file path
-// 	content, err := os.ReadFile(m.migrationsPath)
-// 	if err != nil {
-// 		return fmt.Errorf("failed to get migrations by path '%s'", m.migrationsPath)
-// 	}
-
-// 	// Execute the SQL migrations
-// 	_, err = m.executor.Exec(string(content))
-// 	if err != nil {
-// 		return fmt.Errorf("execute migrations: %w", err)
-// 	}
-
-// 	return nil
-// }
-
-// func (m *Migrator) Apply() error {
-// 	// Apply all available migrations
-// 	if err := m.m.Up(); err != nil && err != migrate.ErrNoChange {
-// 		return err
-// 	}
-
-// 	return nil
-// }
